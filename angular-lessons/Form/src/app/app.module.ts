a2043e2bfbaa5c1ff08bf15caf8342d1e39b8170
Adrion10 <lacehila@gmail.com>
2022-08-24 22:13:51 +0200

    imp{  ReactiveFormsModule } from '@angular/forms';

--- angular-lessons/Form/src/app/app.module.ts
+++ angular-lessons/Form/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
