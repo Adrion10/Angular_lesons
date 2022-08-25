9fe4231ed7d49a209aa67d1acbdfb309c5e88bdb
Adrion10 <lacehila@gmail.com>
2022-08-25 23:21:10 +0200

    Validators.minLength(3),

--- angular-lessons/Form/src/app/app.component.ts
+++ angular-lessons/Form/src/app/app.component.ts
@@ -14,7 +14,10 @@ export class AppComponent {
   title1 = 'Reactiv Form In Angular';
   loginForm1 = new FormGroup({
     user: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
   });
   loginUser1() {
     console.log(this.loginForm1.value);
