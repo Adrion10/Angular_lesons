45a1bd495c7fb42fb4c4a726233002be44de9bda
Adrion10 <lacehila@gmail.com>
2022-08-28 21:52:35 +0200

    ng g directive redEl

--- angular-lessons/Form/src/app/app.module.ts
+++ angular-lessons/Form/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RedElDirective } from './red-el.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RedElDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
