b3c89ab756f63428d5244018dc990260fbd45846
Adrion10 <lacehila@gmail.com>
2022-08-28 22:11:16 +0200

    ng g c user

--- angular-lessons/routing-blog/src/app/app.module.ts
+++ angular-lessons/routing-blog/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
