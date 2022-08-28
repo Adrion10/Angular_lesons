1fdbad3264c7a1f4cfb0040027d8fc848e13a9f1
Adrion10 <lacehila@gmail.com>
2022-08-28 22:26:28 +0200

    <a routerLink="user">User</a>

--- angular-lessons/routing-blog/src/app/app-routing.module.ts
+++ angular-lessons/routing-blog/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     component: AboutComponent,
     path: 'about',
+  },
+  {
+    component: UserComponent,
+    path: 'user',
+  },
+
+  {
+    component: HomeComponent,
+    path: '',
   },
 ];
 
