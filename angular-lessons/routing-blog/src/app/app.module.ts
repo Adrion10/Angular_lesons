450500fc56765f323a5d0d922ec22763f010e194
Adrion10 <lacehila@gmail.com>
2022-08-29 23:35:11 +0200

    404 Page

--- angular-lessons/routing-blog/src/app/app.module.ts
+++ angular-lessons/routing-blog/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UserComponent } from './user/user.component';
+import { NoPageComponent } from './no-page/no-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    UserComponent
+    UserComponent,
+    NoPageComponent
   ],
   imports: [
     BrowserModule,
