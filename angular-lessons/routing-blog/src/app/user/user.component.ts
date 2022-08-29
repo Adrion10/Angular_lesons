1a185bd01fdfd98f9cbcf81c2bd4542d5f410f19
Adrion10 <lacehila@gmail.com>
2022-08-29 23:30:23 +0200

    userId

--- angular-lessons/routing-blog/src/app/user/user.component.ts
+++ angular-lessons/routing-blog/src/app/user/user.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  userId: string | null = null;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    console.log('user id is ', this.route.snapshot.paramMap.get('id'));
+    this.userId = this.route.snapshot.paramMap.get('id');
   }
-
 }
