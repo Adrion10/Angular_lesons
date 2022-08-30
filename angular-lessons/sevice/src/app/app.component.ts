5df1c81bc5e0e2054566c559a0207aa5ded34bb2
Adrion10 <lacehila@gmail.com>
2022-08-30 23:21:56 +0200

    *ngFor="let data of memberData">     {{ data.name }}

--- angular-lessons/sevice/src/app/app.component.ts
+++ angular-lessons/sevice/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'sevice';
+  memberData = [
+    { name: 'Lace', email: 'lace@gmail.com' },
+    { name: 'Noel', email: 'Noel@gmail.com' },
+    { name: 'Marisola', email: 'Marisola@gmail.com' },
+    { name: 'Rove', email: 'Rove@gmail.com' },
+  ];
 }
