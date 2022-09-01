67eb6d7a3915028cc88e72a750c614dd718ca1e4
Adrion10 <lacehila@gmail.com>
2022-09-01 23:26:14 +0200

    import HttpClientModule

--- angular-lessons/sevice/src/app/app.module.ts
+++ angular-lessons/sevice/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-lessons/sevice/src/app/services/userdata.service.ts
+++ angular-lessons/sevice/src/app/services/userdata.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class UserdataService {
-  url = '';
+  url = 'http://localhost:8080/userdata';
   constructor(private http: HttpClient) {}
   users() {
     // return [
