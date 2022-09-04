211fcc119f12333d4c10183c328c0651eb88062f
Adrion10 <lacehila@gmail.com>
2022-09-04 21:19:21 +0200

    create root module

---
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
