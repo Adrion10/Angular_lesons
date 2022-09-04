ee0415e238d5d8b25810d4374129cdb065358645
Adrion10 <lacehila@gmail.com>
2022-09-04 21:31:20 +0200

    ng g c books

--- Angular_Project/MyApp/src/app/app.module.ts
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BooksComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
