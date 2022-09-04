366977b5806f8946e4b6a5a362d4245f401b6573
Adrion10 <lacehila@gmail.com>
2022-09-04 21:41:33 +0200

    list

--- Angular_Project/MyApp/src/app/books/books.component.ts
+++ Angular_Project/MyApp/src/app/books/books.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  name = 'Clean Code';
+  author = 'Robert C Martin';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
