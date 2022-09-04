fa509ad974a1ba92c50156c47929973e2fdf9151
Adrion10 <lacehila@gmail.com>
2022-09-04 21:26:59 +0200

    app.component.html css

--- Angular_Project/MyApp/src/app/app.module.ts
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [AppComponent],
