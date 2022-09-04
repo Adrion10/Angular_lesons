39e1001de5cd3befc64ccc6264f98c9ac4640e15
Adrion10 <lacehila@gmail.com>
2022-09-04 22:08:58 +0200

    [disabled]='isDisabled'

--- Angular_Project/MyApp/src/app/books/books.component.ts
+++ Angular_Project/MyApp/src/app/books/books.component.ts
@@ -14,6 +14,8 @@ export class BooksComponent implements OnInit {
   name2: string = 'The Pragmatic Programmer';
   author2: string = 'David THomas';
   src2: string = 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg';
+
+  isDisabled: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
