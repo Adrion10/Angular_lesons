a0fab61cfa4a3aba89adc80d8a22d1d0ce815dbb
Adrion10 <lacehila@gmail.com>
2022-09-05 18:15:17 +0200

    import { FormsModule }

--- Angular_Project/MyApp/src/app/app.module.ts
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BooksComponent } from './books/books.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, BooksComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
