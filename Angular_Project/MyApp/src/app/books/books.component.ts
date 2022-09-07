7d28322c601008cf3f47c6e549291859b7e962f8
Adrion10 <lacehila@gmail.com>
2022-09-07 10:24:04 +0200

    style in Angular

--- Angular_Project/MyApp/src/app/books/books.component.ts
+++ Angular_Project/MyApp/src/app/books/books.component.ts
@@ -26,6 +26,19 @@ export class BooksComponent implements OnInit {
       image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
       amount: 35,
     },
+    {
+      name: 'Clean code JavaScript',
+      author: 'James Padolsey ',
+      image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
+      amount: 37,
+    },
+    {
+      name: 'Clean Code',
+      author: 'Robert C Martin',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+      amount: 40,
+    },
   ];
   // name: string = 'Clean Code';
   // author: string = 'Robert C Martin';
