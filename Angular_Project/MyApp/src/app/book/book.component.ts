5c2423b3b078d4952ae3737c17cfc71ac6a7990a
Adrion10 <lacehila@gmail.com>
2022-09-08 13:46:08 +0200

    OnDestroy

--- Angular_Project/MyApp/src/app/book/book.component.ts
+++ Angular_Project/MyApp/src/app/book/book.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  EventEmitter,
+  Output,
+  OnDestroy,
+} from '@angular/core';
 import { Book } from '../Types/Book';
+
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css'],
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit, OnDestroy {
   @Input() book: Book = {} as Book;
   @Output() bookEmitter = new EventEmitter<Book>();
 
   constructor() {}
 
   ngOnInit(): void {}
+  ngOnDestroy(): void {
+    console.log({ onDestroy: 'onDestroy' });
+  }
+
   addToCard() {
     this.bookEmitter.emit(this.book);
   }
