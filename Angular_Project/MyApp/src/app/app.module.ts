6ddbf9b97022307e8ba508ffe53fdc36e55b40f9
Adrion10 <lacehila@gmail.com>
2022-09-08 19:22:03 +0200

    booksModule

--- Angular_Project/MyApp/src/app/app.module.ts
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { BooksComponent } from './books/books.component';
 import { FormsModule } from '@angular/forms';
-import { BookComponent } from './book/book.component';
+
+import { BooksModule } from './books/books.module';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent, BookComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, BooksModule],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Angular_Project/MyApp/src/app/books/books.service.ts
+++ Angular_Project/MyApp/src/app/books/books.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
+// {providedIn: 'root',}
 export class BooksService {
   constructor() {}
 
