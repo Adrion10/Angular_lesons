717948bdec72ada6913d21bb52a1e25b6ab89ba4
Adrion10 <lacehila@gmail.com>
2022-09-09 12:09:57 +0200

    Routes = [{ path: '', component:BooksComponent }]

--- Angular_Project/MyApp/src/app/app.module.ts
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { BooksModule } from './books/books.module';
 import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, CartComponent],
-  imports: [BrowserModule, FormsModule, BooksModule],
+  imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
 
   bootstrap: [AppComponent],
 })
