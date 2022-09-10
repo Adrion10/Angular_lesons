0c1ee741aec9cb93e4a82e585b3f946e3ce8e654
Adrion10 <lacehila@gmail.com>
2022-09-10 23:40:17 +0200

    [(ngModel)]="form.email"         name="email"

--- Angular_Project/MyApp/src/app/auth/login/login.component.ts
+++ Angular_Project/MyApp/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginForm } from 'src/app/Types/Autj';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  email: string = '';
+  form: LoginForm = {
+    email: '',
+    password: '',
+  };
 
   constructor() {}
 
