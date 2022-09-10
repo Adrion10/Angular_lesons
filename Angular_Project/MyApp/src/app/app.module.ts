e8c60684ac9adc34febacb4981520cdb2cf99d70
Adrion10 <lacehila@gmail.com>
2022-09-10 23:20:19 +0200

    FormsModule

--- Angular_Project/MyApp/src/app/app.module.ts
+++ Angular_Project/MyApp/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { BooksModule } from './books/books.module';
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, CartComponent],
-  imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
+  imports: [BrowserModule, BooksModule, AppRoutingModule, AuthModule],
 
   bootstrap: [AppComponent],
 })
